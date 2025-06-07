aee3f6339ca29c72f24de7cc920edd9dc0c79a42
Nguyễn Khánh Linh <62316921+linh14755@users.noreply.github.com>
2025-06-07 12:32:49 +0700

    add user interface

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,31 @@
 
 import { useQuery } from '@tanstack/react-query';
 
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+
 const fetchUsers = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   if (!res.ok) throw new Error('Failed to fetch');
@@ -19,7 +44,7 @@ export default function UsersPage() {
 
   return (
     <ul>
-      {data.map(user => (
+      {data.map((user: User) => (
         <li key={user.id}>{user.name}</li>
       ))}
     </ul>
